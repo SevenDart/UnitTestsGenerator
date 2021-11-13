49a4989e8cab7297de0520557418be099d699d2f
Pavel Slyaz <sir.sevendart@gmail.com>
2021-11-13 22:53:09 +0300

    Created sample library for tests

---
+++ SampleLibrary/ClassA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SampleLibrary
+{
+    public class ClassA
+    {
+        private IDependency _dependency;
+        
+        public ClassA(IDependency dependency, int x)
+        {
+            _dependency = dependency;
+        }
+        
+        
+        public bool Lie()
+        {
+            return false;
+        }
+
+        public int Return3(int x)
+        {
+            return 3;
+        }
+
+        public void SayTime(DateTime now)
+        {
+            Console.WriteLine(now);
+        }
+    }
+}
