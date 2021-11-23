fec772ad5123b7dfa500b167573800a9f1aaa605
Pavel Slyaz <sir.sevendart@gmail.com>
2021-11-23 20:28:19 +0300

    added test file with two classes

---
+++ SampleLibrary/ClassC.cs
@@ -0,0 +1,18 @@
+﻿namespace SampleLibrary
+{
+    public class ClassC
+    {
+        public int Return5()
+        {
+            return 5;
+        } 
+    }
+
+    public class ClassCD
+    {
+        public bool ReturnTrue()
+        {
+            return true;
+        }
+    }
+}
