e2e2f565df892f4d61de03c46f207aa00c569294
Pavel Slyaz <sir.sevendart@gmail.com>
2021-12-07 17:05:56 +0300

    Added sample project

---
+++ SampleProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnitTestsGeneratorLibrary;
+
+namespace SampleProject
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var generator = new UnitTestsGenerator();
+
+            var generatorConfig = new GeneratorConfig();
+            
+            generatorConfig.Filenames.Add(@"D:\CODE\Csharp\UnitTestsGenerator\SampleLibrary\ClassA.cs");
+            generatorConfig.Filenames.Add(@"D:\CODE\Csharp\UnitTestsGenerator\SampleLibrary\ClassB.cs");
+            generatorConfig.Filenames.Add(@"D:\CODE\Csharp\UnitTestsGenerator\SampleLibrary\ClassC.cs");
+            generatorConfig.EndpointFolder = @"D:\CODE\Csharp\UnitTestsGenerator\SampleLibrary.Tests\";
+            
+            await generator.GenerateTests(generatorConfig);
+        }
+    }
+}
